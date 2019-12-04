faca83bd59e008a35908aabf5224777943da4cdd
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-12-04 09:35:09 +0100

    basic usage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+
+const intAvailable = [1,2,3,4,5,6,7,8,9,0];
 
 function App() {
+
+  const [firstValue, setFirstValue] = useState(0);
+  const [secondValue, setSecondValue] = useState(0);
+  const [inputValue, setInputValue] = useState(0);
+  const [mathSymbol, setMathSymbol] = useState();
+  const [firstValueDone, setFirstValueDone] = useState(false);
+
+  useEffect(() => {
+    firstValueDone ? setSecondValue(inputValue) : setFirstValue(inputValue);
+  }, [firstValueDone, inputValue]);
+
+  const handleValue = (val) => {
+    setInputValue(prevVal => {
+      return prevVal === 0 ? val : prevVal + val;
+    });
+  }
+
+  const resetValue = () => {
+    setInputValue(0);
+    setFirstValue(0);
+    setSecondValue(0);
+  }
+
+  const handleMathSymbol = (val) => {
+    setFirstValue(inputValue);
+    setMathSymbol(val);
+    setFirstValueDone(true);
+    setInputValue(0);
+  }
+
+  const calculate = () => {
+    let result;
+    setSecondValue(inputValue);
+    if(mathSymbol === '+') result = parseInt(firstValue) + parseInt(secondValue);
+    else if(mathSymbol === '-') result = parseInt(firstValue) - parseInt(secondValue);
+    else if(mathSymbol === '*') result = parseInt(firstValue) * parseInt(secondValue);
+    else if(mathSymbol === '/') result = parseInt(firstValue) / parseInt(secondValue);
+    setInputValue(result);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div type="text" className="input">{inputValue}</div>
+      <div className="calcBtns">
+        <div className="IntButtons">
+          {intAvailable.map((el, idx) => {
+            return <button className={`intbtn-${idx}`} key={idx} value={el} onClick={(e) => handleValue(e.target.value)}>{el}</button>
+          })}
+          <button onClick={resetValue}>C</button>
+          <button onClick={calculate}>=</button>
+        </div>
+        <div className="MathBtns">
+          <div className="sumBtn">
+            <button value='+' onClick={(e) => handleMathSymbol(e.target.value)}>+</button>
+          </div>
+          <div className="substractBtn">
+            <button value='-' onClick={(e) => handleMathSymbol(e.target.value)}>-</button>
+          </div>
+          <div className="multiBtn">
+            <button value='*' onClick={(e) => handleMathSymbol(e.target.value)}>*</button>
+          </div>
+          <div className="divBtn">
+            <button value='/' onClick={(e) => handleMathSymbol(e.target.value)}>/</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
